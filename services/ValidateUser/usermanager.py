527d0b2d249f935c16c805d40f5983eee5607e15
Rowida <rowidanagah462000@gmail.com>
2022-11-26 03:32:24 +0200

    add user repo

---
+++ services/ValidateUser/usermanager.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+sys.path.append("../../src/apidoc/")
+
+from user import UserFolderSetup
+
+os.chdir("../../res")
+
+DIR_PATH = os.path.dirname(os.path.realpath('__file__'))
+
+DIR_PATH += "/users/"
+
+class User:
+	def __init__(self):
+		self.setup = UserFolderSetup()
+		self.create_initial_setup()
+
+	def create_initial_setup(self):
+		if not self.setup.check_init():
+			self.setup.initial_setup()
+	
+	def check_user(self, user_name):
+		return self.setup.check_user(DIR_PATH + user_name)
+
+	def check_user_history(self, user_name):
+		try:
+			res = self.setup.check_user_history(DIR_PATH+user_name)
+			return res if res else "No history provided"
+		except:
+			return "not a valid user name"
+
+	def create_user(self, user_name):
+		""" validate first that this user doesn't exist -> create
+		"""
+		#path = DIR_PATH + "/users/{}".format(user_name)
+		path = DIR_PATH + "/{}".format(user_name)
+		if not self.setup.check_user(path):
+			self.setup.create_user(path)
+
+	def rename(self, old_name, new_name):
+		res = DIR_PATH + "/{}".format(old_name)
+		dst = DIR_PATH + "/{}".format(new_name)
+		try: 
+			# make sure if the new username doesn't already exist.
+			self.setup.update_user(res,dst)
+		except:
+			print("UserName already exists")
--- src/apidoc/user.py
+++ src/apidoc/user.py
@@ -1,36 +1,26 @@
-import os
 
-
-os.chdir("../../res")
-
-DIR_PATH = os.path.dirname(os.path.realpath('__file__'))
-
-class User:
+class UserFolderSetup:
+	""" It's a more general repo to magage folder
+	 folder path & structure per user 
+	"""
 	def check_init(self):
 		""" Check is the res folder that hold 
 		users' data exist """
-		if not os.path.isdir("users"):
-			self.initial_setup()
-		return True
+		return os.path.isdir("users")
 
 	def initial_setup(self):
 		os.mkdir("users")
 
 	def check_user(self,user_name):
-		if not os.path.isdir("/users/{}".format(user_name)):
-			self.create_user("/users/{}".format(user_name))
-		return True
+		return os.path.isdir(user_name)
 
 	def create_user(self,user_name):
-		os.mkdir(DIR_PATH + user_name)
+		os.mkdir(user_name)
 
 	def check_user_history(self, user_name):
-		return len(os.listdir(DIR_PATH + "/users/{}".format(user_name)))
-		
-	def update_user(self, user_name, new_user_name):
-		# to rename user's folder 
-		file_name = DIR_PATH +"/users/{}".format(user_name)
-		dst = DIR_PATH +"/users/{}".format(new_user_name)
-		os.rename(file_name, dst)
+		return os.listdir(user_name)
 
-		
+	def update_user(self, user_name, new_user_name):
+		os.rename(user_name, new_user_name)
+
+
